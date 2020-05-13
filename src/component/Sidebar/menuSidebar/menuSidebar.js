be6cc8693a0abbec132bfe17ef80eb9469b2aad8
Tina <tinahomayouni@gmail.com>
2020-05-13 14:41:17 +0430

    Done sidebar

--- src/component/Sidebar/menuSidebar/menuSidebar.js
+++ src/component/Sidebar/menuSidebar/menuSidebar.js
@@ -1,12 +1,77 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { faAddressBook, faCarrot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./menuSidebar.scss";
+
 
 function MenuSidebar() {
   return (
     <aside>
       <h5>MENU</h5>
       <ul>
-        <li>krgqiwu</li>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faCarrot} />
+              About us
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faAddressBook} />
+              Contact us
+          </Link>
+        </li>
       </ul>
+
+      <h5>MENU</h5>
+      <ul>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faCarrot} />
+              About us
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faAddressBook} />
+              Contact us
+          </Link>
+        </li>
+      </ul>
+
+      <h5>MENU</h5>
+      <ul>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faCarrot} />
+              About us
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faAddressBook} />
+              Contact us
+          </Link>
+        </li>
+      </ul>
+
+      <h5>MENU</h5>
+      <ul>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faCarrot} />
+              About us
+          </Link>
+        </li>
+        <li>
+          <Link>
+            <FontAwesomeIcon className="custom" icon={faAddressBook} />
+              Contact us
+          </Link>
+        </li>
+      </ul>
+
     </aside>
   )
 }
