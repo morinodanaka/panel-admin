6339ece528c598e50d6f05235a410246d5fd1ca4
Tina <tinahomayouni@gmail.com>
2020-06-10 16:36:21 +0430

    thunk and axios install

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import { Provider } from 'react-redux';
 import { rootReducer } from './redux/reducer';
 import logger from 'redux-logger'
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer , applyMiddleware(logger));
+
+const store = createStore(rootReducer , composeWithDevTools(applyMiddleware(logger , thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
